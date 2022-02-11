717d2ba036736ee9eec78597d95dd622e88ab81f
GetPsyched <priyanshutr@gmail.com>
2022-02-11 16:17:50 +0400

    Add source files
    Add main html, css and javascript code

---
+++ src/main.js
@@ -0,0 +1,9 @@
+function inputFile() {
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = _ => {
+        let files = Array.from(input.files);
+        console.log(files);
+    };
+    input.click();
+}
